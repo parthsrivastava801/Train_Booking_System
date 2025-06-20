76f79bd6857abfa82e251b32afc2221221df5683
parthsrivastava801 <parthsrivastva801@gmail.com>
2025-06-21 01:00:24 +0530

    added serializers

---
+++ train_booking/booking/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import Train, Booking
+
+
+
+class TrainSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Train
+        fields = '__all__'
+
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+        read_only_fields = ['user', 'booking_timestamp']
+
+    def validate(self, data):
+        train = data['train']
+        seat_number = data['seat_number']
+
+        # Check if seat number is within valid range
+        if seat_number < 1 or seat_number > train.total_seats:
+            raise serializers.ValidationError(f"Seat number must be between 1 and {train.total_seats}.")
+
+        # Check if seat is already booked
+        if Booking.objects.filter(train=train, seat_number=seat_number).exists():
+            raise serializers.ValidationError(f"Seat number {seat_number} is already booked for this train.")
+
+        return data
