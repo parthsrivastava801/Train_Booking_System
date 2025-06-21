3d8441f797506be0752171e0cefcb00b740c4beb
parthsrivastava801 <parthsrivastva801@gmail.com>
2025-06-21 21:27:55 +0530

    registering models to admin page

--- train_booking/booking/admin.py
+++ train_booking/booking/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from django.contrib import admin
+from .models import Train, Booking
+
+
+@admin.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ('name', 'source', 'destination', 'departure_time', 'total_seats', 'seats_available')
+    search_fields = ('name', 'source', 'destination')
+    list_filter = ('source', 'destination', 'departure_time')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'train', 'seat_number', 'booking_timestamp')
+    search_fields = ('user__username', 'train__name')
+    list_filter = ('booking_timestamp', 'train')
